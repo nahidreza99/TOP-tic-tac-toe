ed81ce012ca62096e33cf0eeeb949197ed24899d
Nahid Reza <nahidreza99@gmail.com>
2023-03-30 16:14:16 +0600

    Game elements

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
 const gameBoard = (() => {
     let board = new Array(9);
+    
+    const getGameBoard = () => {
+        return gameBoard;
+    }
+
+    const updateCell = (i,sign) => {
+        board[i] = sign;
+    }
+
+    return{
+        getGameBoard,
+        updateCell,
+    }
+})();
+
+const player = (sign) =>{
+    let _sign = sign;
+    let score = 0;
+};
+
+const displayControl = (() => {
+    //
 
     return{
 
     }
 })();
 
-const displayControl = (() => {
-    
+const gameController = (() => {
+    //const playRound = () => 
+    const cells = document.getElementsByClassName('block');
+    const placeSign = (sign) => {
+
+    }
+
+    const clickCell = () => {
+        for(let i = 0; i<9; i++){
+            cells[i].onclick = function() {
+                cells[i].innerHTML = 'x';
+                console.log('working');
+            }
+        }
+    }
+
+    return{
+        clickCell,
+    }
 })();
+
+gameController.clickCell();
